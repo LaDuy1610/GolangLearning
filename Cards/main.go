645ecce1287cebe6e07e59db0bd3f553970e26c3
LaDuy <laduy2208@gmail.com>
2023-12-20 17:02:53 +0700

    edit main function in Cards

--- Cards/main.go
+++ Cards/main.go
@@ -1,8 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 	hand, remainingCards := deal(cards, 5)
+	fmt.Println("Hand:")
 	hand.print()
+	fmt.Println("RemainigCards:")
 	remainingCards.print()
 }
