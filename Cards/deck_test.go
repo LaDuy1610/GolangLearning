f52d99c0261a7785955351b312385991eb6768c2
LaDuy <laduy2208@gmail.com>
2023-12-21 15:48:01 +0700

    finish Cards project

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -18,6 +21,17 @@ func TestNewDeck(t *testing.T) {
 	}
 }
 
-func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
 
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_deckTesting")
 }
