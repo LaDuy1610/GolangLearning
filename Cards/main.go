9ba4094950b8e12ba21488b3e5d07eac11a40050
LaDuy <laduy2208@gmail.com>
2023-12-21 15:26:19 +0700

    add Go Test

--- Cards/main.go
+++ Cards/main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
-	fmt.Println("Hand:")
-	hand.print()
-	fmt.Println("RemainigCards:")
-	remainingCards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// fmt.Println("Hand:")
+	// hand.print()
+	// fmt.Println("RemainigCards:")
+	// remainingCards.print()
+
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+
+	cards.shuffle()
+	cards.print()
 }
